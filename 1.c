04c194caf31bc4b1ed1ac2f6e345026f035b7d53
DibyamSahoo <82827579+DibyamSahoo@users.noreply.github.com>
2021-04-27 12:09:50 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,17 @@
+printPreorder(node->left); 
+	printPreorder(node->right); 
+}
+/*
+struct node *search(root, a){
+	if(a==root->data){
+		return root;
+	}
+	if(a< root->data)
+		search(root->left);
+	
+}
+*/
+
+
+void main(){
+	int tree[] = {22,31,44,54,67,33,23,46,88.2.3};
